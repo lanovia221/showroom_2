cc3cdcec0e1b04e4313fb15e5f10fef3a5b6741a
lanovia221 <lanovialiem01@student.ciputra.ac.id>
2023-11-14 16:38:48 +0700

    delete_data for move controller

--- app/delete-data.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../auth/koneksi.php';
-$customerId		=$_GET['customerId'];
-$query = mysqli_query($mysqli, "DELETE FROM customer WHERE customerId='$customerId'");
-
-
-header ("location:tabel_customer.php");
-
-?>
--- app/tambah-data.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../auth/koneksi.php';
-
-$query = mysqli_query($mysqli, "INSERT INTO customer (name, address, phoneNumber, idCard) 
-VALUES ('', '', '', '')");
-
-header ("location:tabel_customer.php");
-
-?>
